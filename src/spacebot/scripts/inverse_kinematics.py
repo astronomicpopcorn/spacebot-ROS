94be8cab4aa46312222d11dd1015ad7601a49b89
luckystriker16 <75381524+luckystriker16@users.noreply.github.com>
2026-05-27 22:29:50 +0200

    erster Entwurf inverse_kinematic Node
    - Soll einen Node erstellen, der sowohl listener als auch publisher ist
    - Soll Twist Message annehmen und dann mulitfloatarray mit Winkelgeschwindigkeiten ausgeben.
    - Noch nicht getestet

--- src/spacebot/scripts/inverse_kinematics.py
+++ src/spacebot/scripts/inverse_kinematics.py
@@ -2,5 +2,57 @@
 
 import rospy
 
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float32
+from std_msgs.msg import Float32MultiArray
+
+
+
+#Funktionen für die inverse Kinematik
+
+r = 0.2 #[m] Radius Reifen
+b = 0.5 #[m] Radabstand
+pub = None #Globale variable für den ROS Publisher
+
+velocity_L = 0.0
+velocity_R = 0.0
+
+def caluculateInverseKinematic(data):
+    global velocity_L, velocity_R
+    w = data.angular.z
+    v = data.linear.x
+    velocity_L = getAngularVelocityLeft(v, w)
+    velocity_R = getAngularVelocityRight(v, w)
+
+#Gibt die Winkelgeschw. des r. Rades zurück 
+def getAngularVelocityRight(v, w): # v=lineare geschw.; w=drehgeschw.
+    return (v+w*(b/2))/r
+
+def getAngularVelocityLeft(v, w): # v=lineare geschw.; w=drehgeschw.
+    return (v-w*(b/2))/r
+
+
+
+#Funktionen für die ROS-Kommunikation
+
+def initilizeROSNode():
+    global pub
+    rospy.init_node("inverse_kinematics")
+    rospy.Subscriber("spacebot_twist_input", Twist, caluculateInverseKinematic)
+    pub = rospy.Publisher('spacebot_angular_velocities', Float32MultiArray, queue_size=10)
+    rate = rospy.Rate(10)  # 10 Hz
+
+    rospy.loginfo("Node started")
+
+    while not rospy.is_shutdown():
+        postMessage()
+        rate.sleep()
+
+def postMessage():
+    msg = Float32MultiArray()
+    msg.data = [velocity_L, velocity_R]
+    pub.publish(msg)
+
 if __name__ == '__main__':
+    initilizeROSNode()
     print("Ich bin der inverse Kinematics script! :)")
